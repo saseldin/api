02e349a328aa68259f04a4de51c77149625a514c
mustafa <saseldin2018@gmail.com>
2020-01-15 07:42:35 -0500

    relationships is done

--- app/model/Product.php
+++ app/model/Product.php
@@ -3,8 +3,10 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\model\Review;
 class Product extends Model
 {
-    //
+    public function reviews(){
+      return $this->hasMany(Review::class);
+    }
 }
--- app/model/Review.php
+++ app/model/Review.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\model;
-
+use App\model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+  public function product(){
+    return $this->belongsTo(Product::class);
+  }
 }

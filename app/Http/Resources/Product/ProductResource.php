7559c26b05163dff1fb1c84ca25bcc0435650d56
mustafa <saseldin2018@gmail.com>
2020-01-16 04:48:03 -0500

    modifying the product resources

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
           "name" =>$this->name,
           'description' =>$this->details,
           "price" => $this->price,
-          "stock" =>$this->stock,
+          "totalPrice" => round((1-($this->discount/100))*$this->price,2),
+          "stock" =>$this->stock == 0 ? 'out of stock' : $this->stock,
           "discount" =>$this->discount,
+          "rating" => $this->reviews->count() > 0 ?
+          round($this->reviews->sum('star')/$this->reviews->count(),2) : "not rated yet",
+          "herf" =>[
+            "reviews" =>route("reviews.index",$this->id)
+          ],
         ];
     }
 }
